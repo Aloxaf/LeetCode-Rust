f5577c53113ed30108ad1622b3ae5d8f4bb86e1d
Aloxaf <bailong104@gmail.com>
2018-12-30 21:06:28 +0800

    更改算法

--- src/merge_two_sorted_lists.rs
+++ src/merge_two_sorted_lists.rs
@@ -1,37 +1,39 @@
 use crate::ListNode;
 
+use std::mem;
+
 impl Solution {
     pub fn merge_two_lists(
-        l1: Option<Box<ListNode>>,
-        l2: Option<Box<ListNode>>,
+        mut l1: Option<Box<ListNode>>,
+        mut l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        let mut head = Box::new(ListNode::new(0));
-        let mut ref_head = &mut head;
+        let mut dummy = Box::new(ListNode::new(0));
+        let mut cur = &mut dummy;
 
-        let (mut l1, mut l2) = (l1.as_ref(), l2.as_ref());
-        while l1.is_some() || l2.is_some() {
-            let (x, y) = (l1.map(|n| n.val), l2.map(|n| n.val));
-
-            let mut l1_next = |x: i32| {
-                l1 = l1.map(|n| n.next.as_ref()).unwrap_or(None);
-                x
+        let (mut l1, mut l2) = (&mut l1, &mut l2);
+        while let (Some(n1), Some(n2)) = (l1.as_ref(), l2.as_ref()) {
+            let (v1, v2) = (n1.val, n2.val);
+            let x = if v1 < v2 {
+                l1 = &mut l1.as_mut().unwrap().next;
+                v1
+            } else {
+                l2 = &mut l2.as_mut().unwrap().next;
+                v2
             };
-            let mut l2_next = |y: i32| {
-                l2 = l2.map(|n| n.next.as_ref()).unwrap_or(None);
-                y
-            };
-            let n = match (x, y) {
-                (Some(x), Some(y)) => if x < y { l1_next(x) } else { l2_next(y) },
-                (Some(x), None) => l1_next(x),
-                (None, Some(y)) => l2_next(y),
-                _ => unreachable!()
-            };
-
-            ref_head.next = Some(Box::new(ListNode::new(n)));
-            ref_head = ref_head.next.as_mut().unwrap();
+            // 这个地方我本来试图用 mem::swap 避免新建节点, 然而未能成功战胜 borror checker,
+            // 如果有谁实现了还望不啬赐教
+            cur.next = Some(Box::new(ListNode::new(x)));
+            cur = cur.next.as_mut().unwrap();
         }
 
-        head.next
+        // 如果 l1 仍然有剩余节点, 直接将 cur.next 指向 l1, 反之亦然
+        if l1.is_some() {
+            mem::swap(&mut cur.next, l1);
+        } else {
+            mem::swap(&mut cur.next, l2);
+        }
+
+        dummy.next
     }
 }
 
