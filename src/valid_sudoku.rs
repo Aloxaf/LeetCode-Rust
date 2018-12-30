0c92b13b6e77dadeec75d51731ed233dc6227618
Aloxaf <bailong104@gmail.com>
2018-12-30 14:21:43 +0800

    更简洁的算法

--- src/valid_sudoku.rs
+++ src/valid_sudoku.rs
@@ -37,6 +37,29 @@ impl Solution {
         }
         true
     }
+
+    /// 妙啊妙啊
+    /// https://leetcode.com/problems/valid-sudoku/discuss/15472/Short%2BSimple-Java-using-Strings
+    /// 对于 i 行 j 列的数字 c, 将 (None, c, j) (i, c, None) (i / 3, c, j / 3) 插入一个 HashSet 中
+    /// 若插入时发现键已存在, 则说明对应 列/行/九宫格 存在重复数字
+    /// 不过竟然更慢了? 总感觉是 Rust Hash 算法的锅...
+    pub fn is_valid_sudoku_hashset(board: Vec<Vec<char>>) -> bool {
+        use std::collections::HashSet;
+        let mut seen = HashSet::new();
+        for (i, l) in board.iter().enumerate() {
+            for (j, &m) in l.iter().enumerate() {
+                if m != '.' {
+                    if !seen.insert((None, m, Some(j)))
+                        || !seen.insert((Some(i), m, None))
+                        || !seen.insert((Some(i / 3), m, Some(j / 3)))
+                    {
+                        return false
+                    }
+                }
+            }
+        }
+        true
+    }
 }
 
 pub struct Solution;
@@ -64,6 +87,39 @@ mod test {
 
         assert_eq!(
             Solution::is_valid_sudoku(vec![
+                vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+                vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+                vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+                vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+                vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+                vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+                vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+                vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+                vec!['.', '.', '.', '.', '8', '.', '.', '7', '9']
+            ]),
+            true
+        );
+    }
+
+    #[test]
+    fn test_hashset() {
+        assert_eq!(
+            Solution::is_valid_sudoku_hashset(vec![
+                vec!['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+                vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+                vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+                vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+                vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+                vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+                vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+                vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+                vec!['.', '.', '.', '.', '8', '.', '.', '7', '9']
+            ]),
+            false
+        );
+
+        assert_eq!(
+            Solution::is_valid_sudoku_hashset(vec![
                 vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
                 vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
                 vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
