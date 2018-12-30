25b2c28183b837ad75d2190147bc6f01919f6a73
Aloxaf <bailong104@gmail.com>
2018-12-30 20:06:04 +0800

    修复传入 std::i32::MAX 导致的死循环问题

--- src/merge_two_sorted_lists.rs
+++ src/merge_two_sorted_lists.rs
@@ -10,16 +10,23 @@ impl Solution {
 
         let (mut l1, mut l2) = (l1.as_ref(), l2.as_ref());
         while l1.is_some() || l2.is_some() {
-            let (x, y) = (l1.map(|n|n.val), l2.map(|n|n.val));
-            let (x, y) = (x.unwrap_or(std::i32::MAX), y.unwrap_or(std::i32::MAX));
+            let (x, y) = (l1.map(|n| n.val), l2.map(|n| n.val));
 
-            let n = if x < y {
-                l1 = l1.map(|n|n.next.as_ref()).unwrap_or(None);
+            let mut l1_next = |x: i32| {
+                l1 = l1.map(|n| n.next.as_ref()).unwrap_or(None);
                 x
-            } else {
-                l2 = l2.map(|n|n.next.as_ref()).unwrap_or(None);
+            };
+            let mut l2_next = |y: i32| {
+                l2 = l2.map(|n| n.next.as_ref()).unwrap_or(None);
                 y
             };
+            let n = match (x, y) {
+                (Some(x), Some(y)) => if x < y { l1_next(x) } else { l2_next(y) },
+                (Some(x), None) => l1_next(x),
+                (None, Some(y)) => l2_next(y),
+                _ => unreachable!()
+            };
+
             ref_head.next = Some(Box::new(ListNode::new(n)));
             ref_head = ref_head.next.as_mut().unwrap();
         }
@@ -31,7 +38,7 @@ impl Solution {
 pub struct Solution;
 
 #[cfg(test)]
-mod test {
+mod tests {
     use crate::linkedlist;
     use super::Solution;
 
@@ -40,5 +47,12 @@ mod test {
         let l1 = linkedlist![1, 2, 4];
         let l2 = linkedlist![1, 3, 4];
         assert_eq!(Solution::merge_two_lists(l1, l2), linkedlist![1, 1, 2, 3, 4, 4]);
+    }
+
+    #[test]
+    fn test2() {
+        let l1 = linkedlist![1, 2, 2147483647];
+        let l2 = linkedlist![1, 2, 4];
+        assert_eq!(Solution::merge_two_lists(l1, l2), linkedlist![1, 1, 2, 2, 4, 2147483647]);
     }
 }
